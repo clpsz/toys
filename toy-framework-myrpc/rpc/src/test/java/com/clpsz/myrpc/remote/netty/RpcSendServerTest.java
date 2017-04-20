e2098ef3f8a86f574d145421481cc7fe28229550
Pengpeng Zuo <pengclpsz@gmail.com>
2017-04-20 11:36:38 +0800

    删除netty测试使用中多余代码

--- toy-framework-myrpc/rpc/src/test/java/com/clpsz/myrpc/remote/netty/RpcSendServerTest.java
+++ toy-framework-myrpc/rpc/src/test/java/com/clpsz/myrpc/remote/netty/RpcSendServerTest.java
@@ -1,12 +1,9 @@
 package com.clpsz.myrpc.remote.netty;
 
-import com.clpsz.myrpc.invoke.model.RequestMsg;
 import com.clpsz.myrpc.invoke.proxy.ClientProxyFactory;
 import com.clpsz.myrpc.invoke.sample.SampleAddService;
-import io.netty.handler.codec.serialization.ObjectEncoder;
 import org.junit.Test;
 
-import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -18,22 +15,11 @@ public class RpcSendServerTest {
         RpcSendServer rpcSendServer = new RpcSendServer();
         rpcSendServer.start();
 
-        ChannelSendHandler handler = SendHandlerHolder.getInstance().getSendHandler();
-        System.out.println(handler);
-        RequestMsg request = new RequestMsg();
-        request.setMsgId(UUID.randomUUID().toString());
-
-
         SampleAddService proxy = ClientProxyFactory.getNettyProxy(SampleAddService.class);
-        int result = proxy.add(3, 114);
+        int result = proxy.add(3, 3000);
 
         System.out.println(result);
-//
-//        MsgCallBack msgCallback = handler.sendRequest(request);
-//        Object result = msgCallback.get();
-//        System.out.println(result);
 
-//        handler.close();
 
         TimeUnit.SECONDS.sleep(3);
     }
